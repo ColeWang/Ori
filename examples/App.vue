215490befdc7076080382384dc3817a33f17be30
jokerCole <cole.wang@wetax.com.cn>
2021-03-01 16:21:55 +0800

    Hello World

---
+++ examples/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    setup () {
+      console.log(123)
+    }
+  }
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,80 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/dist/plugin').default
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   mode: 'development',
-  entry: '',
+  entry: './examples/index.js',
   module: {
-    rules: []
+    rules: [
+      {
+        test: /\.(vue)$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {}
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                sourceMap: true,
+                javascriptEnabled: true,
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true
+            }
+          },
+          'css-loader',
+        ]
+      }
+    ]
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'examples/index.html',
+      filename: 'index.html',
+      inject: true,
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    })
+  ],
   resolve: {
     alias: {}
   },
-  devServer: {},
+  devServer: {
+    historyApiFallback: {
+      rewrites: [{ from: /./, to: '/index.html' }],
+    },
+    disableHostCheck: true,
+    hot: true,
+    open: true,
+  }
 }
